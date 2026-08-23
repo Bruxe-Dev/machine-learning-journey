4f4c04164e2864ec233e1047d766a6f92b0abb1d
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 20:09:15 +0200

    Made some changes

--- multiclass_tensorflow.py
+++ multiclass_tensorflow.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np 
 
 from tensorflow.keras import Input
-from tensorflow.keras import Sequential 
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense 
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.datasets import mnist 
+
