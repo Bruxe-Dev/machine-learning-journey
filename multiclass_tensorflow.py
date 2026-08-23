1a2432a0d80c80aa9009ea61558eb20d63796340
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 19:59:00 +0200

    implemented the relu_derivative and its derivative for foward propagation and backward propagation respectivaly.

--- multiclass_tensorflow.py
+++ multiclass_tensorflow.py
@@ -5,3 +5,9 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.datasets import mnist 
+
+def relu_activation(z):
+    return np.maximum(0,z)
+
+def relu_derivative(z):
+    return (z > 0).astype(float)
