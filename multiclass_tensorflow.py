d3e32d379d32bd76b806c7f31f65825645f44a33
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 20:49:03 +0200

    Chanfged the first Neural Network input layer into a flatten one thta transforms the images directly into a 2D array without the neeed of manually implementing it.

--- multiclass_tensorflow.py
+++ multiclass_tensorflow.py
@@ -4,6 +4,7 @@ import numpy as np
 from tensorflow.keras import Input
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense 
+from tensorflow.keras.layers import Flatten
 from tensorflow.keras.datasets import mnist 
 
 (X_train, Y_train), (X_test, Y_test)= mnist.load_data()
@@ -15,16 +16,12 @@ print(Y_train.shape)
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
-# Reshape the Normalized images (flatten)
-
-X_train = X_train.reshape(X_train.shape[0], -1)
-X_test = X_test.reshape(X_test.reshape[0], -1)
-
 # Make a model 
 
 model  = Sequential ([
-    Input(shape=(784,)),
+    Flatten(input_shape=(28,28)),
     Dense(units=128, activation ="relu"),
     Dense(units=64, activation = "relu"),
     Dense(units=10)
 ])
+
