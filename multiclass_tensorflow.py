d33d053678e112dfb0a5f3a27977d4fdc190beba
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 20:03:15 +0200

    Realized that tensorflow, keras handles the activation functions and their derivatives internallly so i removed them.

--- multiclass_tensorflow.py
+++ multiclass_tensorflow.py
@@ -1,13 +1,8 @@
 import tensorflow as tf 
-import pandas as pd 
+import numpy as np 
+
 from tensorflow.keras import Input
 from tensorflow.keras import Sequential 
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.datasets import mnist 
-
-def relu_activation(z):
-    return np.maximum(0,z)
-
-def relu_derivative(z):
-    return (z > 0).astype(float)
