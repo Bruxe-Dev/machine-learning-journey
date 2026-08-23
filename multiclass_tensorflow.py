7de68bd4c21163a7cbb4d365c0755668396c7b3d
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 20:29:10 +0200

    Loaded the MNIST dataset and then Normalized and flattened the images into 2D matrix that can be processed and then Sequenced the model with Dense Neural Network.

--- multiclass_tensorflow.py
+++ multiclass_tensorflow.py
@@ -4,6 +4,27 @@ import numpy as np
 from tensorflow.keras import Input
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense 
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.datasets import mnist 
 
+(X_train, Y_train), (X_test, Y_test)= mnist.load_data()
+print(X_train.shape)
+print(Y_train.shape)
+
+# Normalize the images 
+
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Reshape the Normalized images (flatten)
+
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_test = X_test.reshape(X_test.reshape[0], -1)
+
+# Make a model 
+
+model  = Sequential ([
+    Input(shape=(784,)),
+    Dense(units=128, activation ="relu"),
+    Dense(units=64, activation = "relu"),
+    Dense(units=10)
+])
