96e56ba502090c35706329c68e8b1e5a57456b1c
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-23 19:36:47 +0200

    Started transforming the raw multiclass numpy model into a Tensorflow built one. THis is to test my self if i really understood the basics or not.

---
+++ multiclass_tensorflow.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np 
+from tensorflow.keras import Input
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import sparse_categorical_crossentropy
