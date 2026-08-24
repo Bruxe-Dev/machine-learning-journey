ce112c38a27a2729bea49d1b1cb767be2cc1f87c
Bruxe-Dev <mnibeza23@gmail.com>
2026-08-24 19:40:41 +0200

    Started getting into things step by step

--- multilabel_numpy.py
+++ multilabel_numpy.py
@@ -1,4 +1,44 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
+import numpy as np 
 
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+def binaryCrossentropy(y,p):
+    return  -(y *np.log(p) + (1-y) * np.log(1-p))
+
+X = np.array([
+    [2, 1],
+    [1, 2],
+    [8, 9],
+    [9, 8]
+])
+
+Y = np.array([
+    [1, 0],
+    [0, 1],
+    [1, 1],
+    [0, 0]
+])
+ 
+W = np.array([
+    [0.5, 0.2],
+    [0.3, 0.8]
+])
+
+b = np.array([0.1,0.2])
+
+Z = X @ W + b 
+
+p = sigmoid(Z)
+
+print("Z:")
+print(Z)
+
+print("P:")
+print(p)
+
+loss = binaryCrossentropy(Y,p)
+
+print("Loss: ")
+print(loss)
+#predictions = (p > 0.5).astype(int)
