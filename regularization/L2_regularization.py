Noe I am trying to find out what i am doing wrong here and i ma really figuring out some bugs here and a bad computation that i did wehn finding the derivative of J with respect to weight, So i am going to correct that and others next.

regularization/L2_regularization.py
@@ -40,6 +40,6 @@ x_poly = np.column_stack([
 ])
 
 
-cost = compute_cost_l2(x_poly,Y,W,b)
+cost = compute_cost_l2(x_poly,Y,W,b,lambda_)
 
 print(f"Cost: {cost}")