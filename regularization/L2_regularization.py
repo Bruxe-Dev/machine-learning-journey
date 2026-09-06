c815f49ee5692a772583653d415b693cc9410041
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-06 16:37:51 +0200

    Made the polynomalized prediction function that takes in the input matrix and transforms them into a column of powered features with ascending degreees and then also accepts  the Weight and bias as other parameters and then return a vectorized matrix multiplication. and i have also set up some dummy data (features, weights and bias)

--- regularization/L2_regularization.py
+++ regularization/L2_regularization.py
@@ -1,2 +1,16 @@
-import tensorflow as  tf 
-from tensorflow.keras import Layer, Sequential
+import numpy 
+
+def predict(X,W,b):
+    x_poly = np.column_stack([
+        x,
+        x**2,
+        x**3,
+        x**4
+    ])
+
+    return np.matmul(x_poly,W)+b
+
+X = np.array([1,2,3,4])
+Y = np.array([2,5,10,17])
+W = np.array([1.0,0.5,0.1])
+b = 1.0
