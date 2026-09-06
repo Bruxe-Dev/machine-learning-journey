8fb96b1c5e5e565171c142422fb65929bb7e5fc3
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-06 17:17:40 +0200

    Made the first foward prop codes

--- regularization/L2_regularization.py
+++ regularization/L2_regularization.py
@@ -1,16 +1,29 @@
 import numpy 
 
-def predict(X,W,b):
-    x_poly = np.column_stack([
-        x,
-        x**2,
-        x**3,
-        x**4
-    ])
+def compute_cost(x_poly,y,W,b):
+    m = x_poly.shape[0]
 
-    return np.matmul(x_poly,W)+b
+    predictions = np.matmul(x_poly,W)+b
 
-X = np.array([1,2,3,4])
-Y = np.array([2,5,10,17])
-W = np.array([1.0,0.5,0.1])
+    error = predictions - y
+
+    cost = (1/(2 * m)) * np.sum(error **2)
+
+    return cost
+
+X = np.array([1,2,3,4,5])
+Y = np.array([2,5,10,17,24])
+W = np.array([1.0,0.5,0.1,0.3])
 b = 1.0
+
+x_poly = np.column_stack([
+    x,
+    x**2,
+    x**3,
+    x**4
+])
+
+
+cost = compute_cost(x_poly,Y,W,b)
+
+print(f"Cost: {cost}")
