d2001777257c79f04c95a175b83a8f98f827fb6c
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-06 18:29:27 +0200

    Now i understood where everything was breaking before

--- regularization/L2_regularization.py
+++ regularization/L2_regularization.py
@@ -1,36 +1,45 @@
 import numpy as np
 
-X = np.array([1,2,3,4,5])
-Y = np.array([2,5,10,17,24])
-W = np.array([1.0,0.5,0.1,0.3])
+X = np.array([1, 2, 3, 4, 5])
+Y = np.array([2, 5, 10, 17, 24])
+
+W = np.array([1.0, 0.5, 0.1, 0.3])
 b = 1.0
 
 lambda_ = 1.0
 
-def compute_cost_l2(x_poly,y,W,b,lambda_):
+
+def compute_cost_l2(x_poly, y, W, b, lambda_):
     m = x_poly.shape[0]
 
-    predictions = np.matmul(x_poly,W)+b
+    predictions = np.matmul(x_poly, W) + b
 
     error = predictions - y
 
+    ordinary_cost = (1 / (2 * m)) * np.sum(error ** 2)
+
     l2_penalty = (lambda_ / (2 * m)) * np.sum(W ** 2)
-    ordinary_cost = (1/(2 * m)) * np.sum(error ** 2)
 
     cost = ordinary_cost + l2_penalty
 
     return cost
 
-def compute_gradient_l2(x_poly,y,errors,lambda_,w):
+
+def compute_gradient_l2(x_poly, y, W, b, lambda_):
     m = x_poly.shape[0]
 
-    dj_dw = (1/m) * np.matmul(errors) * x_poly
+    predictions = np.matmul(x_poly, W) + b
 
-    dj_dw += (lambda_ / m) * w
+    errors = predictions - y
 
-    dj_db = (1/m) * np.sum (errors)
+    dj_dw = (1 / m) * np.matmul(x_poly.T, errors)
 
-    return dj_dw,dj_db
+    dj_dw += (lambda_ / m) * W
+
+    dj_db = (1 / m) * np.sum(errors)
+
+    return dj_dw, dj_db
+
 
 x_poly = np.column_stack([
     X,
@@ -40,6 +49,22 @@ x_poly = np.column_stack([
 ])
 
 
-cost = compute_cost_l2(x_poly,Y,W,b,lambda_)
+cost = compute_cost_l2(
+    x_poly,
+    Y,
+    W,
+    b,
+    lambda_
+)
+
+dj_dw, dj_db = compute_gradient_l2(
+    x_poly,
+    Y,
+    W,
+    b,
+    lambda_
+)
 
 print(f"Cost: {cost}")
+print(f"dj_dw: {dj_dw}")
+print(f"dj_db: {dj_db}")
