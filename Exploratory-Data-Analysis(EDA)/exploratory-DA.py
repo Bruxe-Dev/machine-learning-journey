8b5847464a737a187b7be553c2c0fdfa92e6b3a5
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-07 20:33:25 +0200

    Visulaized the count or total numebr of all the records or examples in the survived column.

--- Exploratory-Data-Analysis(EDA)/exploratory-DA.py
+++ Exploratory-Data-Analysis(EDA)/exploratory-DA.py
@@ -1,6 +1,8 @@
 import seaborn as sb 
 import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd
 
 data = sb.load_dataset("titanic")
 
-print(data.describe().transpose())
+print(data['survived'].value_counts())
