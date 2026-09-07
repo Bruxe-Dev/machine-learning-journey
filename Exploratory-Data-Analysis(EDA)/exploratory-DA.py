7f00f827a26945bbac97b5e696b3a694ceb20e3d
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-07 20:58:30 +0200

    Now I was trying to represent who were on the boat using a pie chart using the plot method with specifying the kinds type as pie

--- Exploratory-Data-Analysis(EDA)/exploratory-DA.py
+++ Exploratory-Data-Analysis(EDA)/exploratory-DA.py
@@ -5,4 +5,22 @@ import pandas as pd
 
 data = sb.load_dataset("titanic")
 
-print(data['survived'].value_counts())
+print(data['class'].value_counts())
+
+p_class = data[['pclass','class']]
+print(p_class.head())
+
+# print(data.describe())
+# sb.histplot(data=data,x='age',bins=20,binrange=(0,80),shrink=0.85)
+# plt.xticks(range(0,86,5))
+
+# SHow passengers who were less than 20 years
+
+less20 = data[data['age'] < 20]
+print(less20.head())
+
+# The categories usding the who column and represent it using a pie chart
+
+categories = data['who']
+categories.value_counts().plot(kind='pie')
+plt.show()
