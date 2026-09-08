4a9064ca4e1c1e7baffb42e683019b5ed54c1864
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-08 11:45:39 +0200

    Implemented the Standard Scaler and learnt that it can be used when data has a standard deviation, while Normalization works on data with no standard deviation (skewed)

--- Data-Analysis(EDA)/feature_scaling.py
+++ Data-Analysis(EDA)/feature_scaling.py
@@ -1,10 +1,11 @@
 from seaborn import load_dataset
 import pandas as pd 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 data = load_dataset('tips')
 print(data.head())
 
+# MinMaxScaler (Normalization)
 num_feats = data[['total_bill','tip','size']]
 
 scaler = MinMaxScaler()
@@ -13,3 +14,10 @@ num_feats = scaler.fit_transform(num_feats)
 
 num_scaled_df = pd.DataFrame(data=num_feats,columns=num_feats.columns)
 print(num_scaled.head())
+
+#Standardisation
+
+stScaler = StandardScaler()
+
+st_num_feats = stScaler.fit_transform(num_feats)
+std_numFeats_df = pd.DataFrame(st_num_feats,columns=st_num_feats.colums())
