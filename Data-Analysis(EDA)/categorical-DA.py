3bcf45f4ce704fd2bbeaa244c0b5de404d32860b
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-08 09:32:11 +0200

    I learnt that Dummies is the pandas implemtntation of One hot encoding and we can use OneHotEncoder skilearn class to peform the same action

--- Data-Analysis(EDA)/categorical-DA.py
+++ Data-Analysis(EDA)/categorical-DA.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import seaborn as sns
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 import matplotlib.pyplot as plt 
 
 data = sns.load_dataset('titanic')
@@ -24,8 +24,13 @@ cats_feats = data[['alive','alone']]
 
 cats_feats_encoded = encoder.fit_transform(cats_feats)
 
-# Pandas Dummies
+# Pandas Dummies (peforming one hot encoding using pandas)
 
 dummies = pd.get_dummies(data['who'], drop_first=True)
 
 data = pd.concat([data.drop('who',axis=1),dummies],axis=1 )
+
+#One-Hot-Encoding
+
+hotEncoder = OneHotEncoder()
+townEncoded = hotEncoder.fit_transform(data[['embark_town']])
