5cf498ea29d5f34f548cd4938e7574fb5f4af14f
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-08 07:54:48 +0200

    Implemented the Mapping method on the class features beacuse it has less categories and i will also apply it to others like sex, etc.

--- Data-Analysis(EDA)/categorical-DA.py
+++ Data-Analysis(EDA)/categorical-DA.py
@@ -3,4 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt 
 
 data = sns.load_dataset('titanic')
-print(data.head())
+print(data['class'].head())
+
+# Mapping method( Going to use it on the Class feature)
+
+map_dict = {
+    'First':0,
+    'Second':1,
+    'Third':2
+}
+
+data['class'] = data['class'].map(map_dict)
