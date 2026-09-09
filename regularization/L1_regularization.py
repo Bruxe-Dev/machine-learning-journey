cbb8aab945138dde89476e72547f0790b4fbad1d
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-09 08:21:31 +0200

    Implemented the cost function with the lasso (l1) regularization penalty

--- regularization/L1_regularization.py
+++ regularization/L1_regularization.py
@@ -1,1 +1,18 @@
 import numpy as np 
+
+def compute_lasso_cost(X, y, w, b, lambda_):
+    m = len(y)
+
+    y_pred = w @ x + b
+
+    errors = y - y_pred
+
+    mse = (1/(2 * m)) * np.sum(errors ** 2)
+
+    l1_penalty = (lambda_ / m) *np.sum(np.abs(w))
+
+    cost = mse + l1_penalty
+
+    return cost 
+
+    
