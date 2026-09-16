f8e74c336a4eb2e2f319317a78d5f15d42e4b2bf
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-16 13:20:11 +0200

    Implemmented and loaded the data and analysed it

--- naive_bayes_v1.py
+++ naive_bayes_v1.py
@@ -1,5 +1,16 @@
 import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sb 
 
-df = pd.read_csv("spam.csv",encoding='latin-1')
+
+df = pd.read_csv("data/spam.csv",encoding='latin-1')[['v1','v2']]
 
 print(df.head())
+
+df.columns = ['label','message']
+
+print(df.head())
+X = df['message']
+Y = df['label']
+
+print(Y.value_counts())
