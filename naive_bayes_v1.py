7724aa940d2b70d6b9b581b699f112688ab12a5a
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-16 14:25:28 +0200

    Implemented the data (word) vectorization

--- naive_bayes_v1.py
+++ naive_bayes_v1.py
@@ -1,7 +1,10 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sb 
+from s
+from sklearn.feature_extraction.text import CountVectorizer
 
+vectorizer = CountVectorizer()
 
 df = pd.read_csv("data/spam.csv",encoding='latin-1')[['v1','v2']]
 
@@ -14,3 +17,7 @@ X = df['message']
 Y = df['label']
 
 print(Y.value_counts())
+
+X_vector = vectorizer.fit_transform(X)
+print(vectorizer.get_feature_names_out())
+print(X_vector.shape)
