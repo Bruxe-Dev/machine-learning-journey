9cfa7297c6c6b4aea3fb1728bc40ab98e4d10b50
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-16 15:16:53 +0200

    Split the data into training and test dataset

--- naive_bayes_v1.py
+++ naive_bayes_v1.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sb 
-from s
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
 vectorizer = CountVectorizer()
@@ -21,3 +21,18 @@ print(Y.value_counts())
 X_vector = vectorizer.fit_transform(X)
 print(vectorizer.get_feature_names_out())
 print(X_vector.shape)
+
+X_train,Y_train,X_test,Y_test  = train_test_split(
+    X_vector,
+    Y,
+    test_size=0.2,
+    random_state=42,
+    stratify=Y
+)
+
+print(f"Training: ",X_train.shape[0])
+print(f"Testing: ",X_test.shape[0])
+print("\n Training Distribution: ")
+print(Y_train.value_counts())
+print("\n Test Distribution: ")
+print(Y_test.value_counts()) 
