7c30faef912fdac1c9fcb41328572670375ff8cc
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-16 19:48:25 +0200

    Fitted and trained  the model to the vectorized words and then calculated its accuracy

--- naive_bayes_v1.py
+++ naive_bayes_v1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sb 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 from sklearn.naive_bayes import MultinomialNB
 
 vectorizer = CountVectorizer()
@@ -25,3 +26,10 @@ X_train,Y_train,X_test,Y_test = train_test_split(
 
 x_train_vectors = vectorizer.fit_transform(X_train)
 X_test_vectors = vectorizer.transform(X_test)
+
+nb_model = MultinomialNB()
+nb_model.fit(x_train_vectors,Y_train)
+
+y_pred = nb_model.predict(X_test_vectors)
+
+accuracy = accuracy_score(y_pred,Y_test)
