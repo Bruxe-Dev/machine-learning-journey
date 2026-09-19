9da7892e3963e8c02bf87fcbb41be6a27bcfc49c
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-19 12:03:27 +0200

    Loaded the dataset and printed its sahape on console to confirm its working

--- grid_search.py
+++ grid_search.py
@@ -3,3 +3,9 @@ import pandas as pd
 from tensorflow.keras.datasets import fashion_mnist 
 from tensorflow.keras import Input,Sequential
 from tensorflow.keras.layers import Dense, Flatten
+
+(X_train,Y_train),(X_test,Y_test) = fashion_mnist.load_data()
+print(f"Shape: {X_train.shape}")
+print(f"Shape: {Y_train.shape}")
+print(f"Shape: {X_test.shape}")
+print(f"Shape: {Y_test.shape}")
