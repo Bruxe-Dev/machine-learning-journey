4edd09ddf9fb5fc3889686b81da6d476aac1a01a
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-19 11:59:34 +0200

    Made all the required imports

---
+++ grid_search.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import pandas as pd 
+from tensorflow.keras.datasets import fashion_mnist 
+from tensorflow.keras import Input,Sequential
+from tensorflow.keras.layers import Dense, Flatten
