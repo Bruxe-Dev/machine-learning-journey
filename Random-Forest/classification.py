64b261a484b911c044e6ff6555a74d26cb38e365
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-21 13:32:30 +0200

    Added categorical data to the transformer

--- Random-Forest/classification.py
+++ Random-Forest/classification.py
@@ -29,7 +29,8 @@ categorical_features = ["Sex"]
 
 preprocessing = ColumnTransformer(
     transformers = [
-        ("num", imputer, numerical_features)
+        ("num", imputer, numerical_features),
+        ("cat",categorical_trans,categorical_features)
     ]
 )
 
@@ -40,8 +41,8 @@ X_train,X_test,Y_train,Y_test = train_test_split(
     test_size=0.2
 )
 
-X_train['Sex'] = hotEncoder.fit_transform(X_train[['Sex']])
-X_train['Age'] = X_train['Age'].fillna(X_train['Age'].median())
+# X_train['Sex'] = hotEncoder.fit_transform(X_train[['Sex']])
+# X_train['Age'] = X_train['Age'].fillna(X_train['Age'].median())
 
 rf_classifier = RandomForestClassifier(n_estimators=50,random_state=42)
 rf_classifier.fit(X_train,Y_train)
