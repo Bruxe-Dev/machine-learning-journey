c3f351000fb0a7d83b593695e7ce649987c0a7f2
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-21 12:14:32 +0200

    Made some debuggs in the classification model.

--- Random-Forest/classification.py
+++ Random-Forest/classification.py
@@ -8,13 +8,13 @@ import warnings
 warnings.filterwarnings('ignore')
 le = LabelEncoder()
 
-titanic_data = pd.read_csv("data/titanic.csv")
+titanic_data = pd.read_csv("../data/titanic.csv")
 titanic_data = titanic_data.dropna(subset=["Survived"])
 
 x = titanic_data[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
 y = titanic_data['Survived']
 
-x['Sex'] = le.fit_tranform(x['Sex'])
+x['Sex'] = le.fit_transform(x['Sex'])
 x['Age'] = x['Age'].fillna(x['Age'].median())
 
 X_train,X_test,Y_train,Y_test = train_test_split(
@@ -30,10 +30,13 @@ rf_classifier.fit(X_train,Y_train)
 y_pred = rf_classifier.predict(X_test)
 
 accuracy = accuracy_score(Y_test,y_pred)
-classification_rep = classification_report(y_true,y_pred)
+classification_rep = classification_report(Y_test,y_pred)
 
 print(f"Accuracy: {accuracy:.2f}")
 print("\nClassification Report:\n", classification_rep)
+
+sample = X_test.iloc[0:1]
+prediction = rf_classifier.predict(sample)
 
 sample_dict = sample.iloc[0].to_dict()
 print(f"\nSample Passenger: {sample_dict}")
