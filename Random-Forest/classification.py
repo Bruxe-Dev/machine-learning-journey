83c5d662c023f29bee28cf24a7b5fdd16bf107bd
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-21 12:09:56 +0200

    Implemented a Radom Forest classification model on the titanic dataset and I did somethings that i need to improve afterwards

---
+++ Random-Forest/classification.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,classification_report
+import warnings
+
+warnings.filterwarnings('ignore')
+le = LabelEncoder()
+
+titanic_data = pd.read_csv("data/titanic.csv")
+titanic_data = titanic_data.dropna(subset=["Survived"])
+
+x = titanic_data[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
+y = titanic_data['Survived']
+
+x['Sex'] = le.fit_tranform(x['Sex'])
+x['Age'] = x['Age'].fillna(x['Age'].median())
+
+X_train,X_test,Y_train,Y_test = train_test_split(
+    x,
+    y,
+    random_state=42,
+    test_size=0.2
+)
+
+rf_classifier = RandomForestClassifier(n_estimators=100,random_state=42)
+rf_classifier.fit(X_train,Y_train)
+
+y_pred = rf_classifier.predict(X_test)
+
+accuracy = accuracy_score(Y_test,y_pred)
+classification_rep = classification_report(y_true,y_pred)
+
+print(f"Accuracy: {accuracy:.2f}")
+print("\nClassification Report:\n", classification_rep)
+
+sample_dict = sample.iloc[0].to_dict()
+print(f"\nSample Passenger: {sample_dict}")
+print(f"Predicted Survival: {'Survived' if prediction[0] == 1 else 'Did Not Survive'}")
