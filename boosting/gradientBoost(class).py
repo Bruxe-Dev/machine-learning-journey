5be3fd6f9cfd04e2194cbb00e45bb7f074418822
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-22 22:12:12 +0200

    Made some changes and added a heatmap for a confusion matrix for the classification model.

--- boosting/gradientBoost(class).py
+++ boosting/gradientBoost(class).py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt 
+import seaborn as sb 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.datasets import load_digits
 
@@ -19,4 +21,21 @@ gbc.fit(X_train,y_train)
 pred_y = gbc.predict(X_test)
 
 acc = accuracy_score(y_test,pred_y)
+cm = confusion_matrix(y_test,pred_y)
+
 print(f"The Accuracy score is: {acc}")
+
+plt.figure(figsize=(8,6))
+
+sb.heatmap(
+    cm,
+    annot=True,
+    cmap="Blues",
+    xticklabels=labels,
+    yticklabels=labels
+)
+
+plt.ylabel("Actual Labels")
+plt.xlabel("Predicted Labels")
+plt.title("Confusion Matrix")
+plt.show()
