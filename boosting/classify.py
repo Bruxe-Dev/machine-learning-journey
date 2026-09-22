5f47a308e8e623f25b6231c06ee51de857ed63cd
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-22 08:35:17 +0200

    Started working on the AdaBoost Classification model.

---
+++ boosting/classify.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score,f1_score,confusion_matrix,precision_recall_curve,recall_score
+from sklearn.datasets import load_iris,make_classification
