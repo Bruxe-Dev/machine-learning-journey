31f54d360c37d3b1e6f4d39ba1bb27ddba48d7b1
Bruxe-Dev <mnibeza23@gmail.com>
2026-09-22 21:47:44 +0200

    implemented a Gradient Boost classifier

---
+++ boosting/gradientBoost(class).py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import load_digits
+
+X,Y = load_digits(return_X_y=True)
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,
+    Y,
+    random_state=42,
+    test_size=0.25
+)
+
+gbc = GradientBoostingClassifier(n_estimators=300,learning_rate=0.05,max_features=5,random_state=100)
+
+gbc.fit(X_train,y_train)
+
+pred_y = gbc.predict(X_test)
+
+acc = accuracy_score(y_test,pred_y)
+print(f"The Accuracy score is: {acc}")
